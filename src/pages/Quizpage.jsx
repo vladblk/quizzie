8cd2c985510e853d84963a85f1b914d70aa46f2f
Vlad Blank <blankvladp@gmail.com>
2022-04-05 17:21:04 +0300

    add confetti for perfect score

--- src/pages/Quizpage.jsx
+++ src/pages/Quizpage.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, forwardRef } from 'react';
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 import Quiz from '../components/Quiz';
 
 function Quizpage() {
     const [quiz, setQuiz] = useState([]);
     const [isOver, setIsOver] = useState(false);
+
+    console.log(quiz);
 
     useEffect(() => {
         fetchQuiz();
@@ -19,7 +22,7 @@ function Quizpage() {
                 id: nanoid(),
                 question: result.question,
 
-                answers: [...result.incorrect_answers, result.correct_answer].sort(function(){ return Math.random() - 0.5}).map(answer => {
+                answers: [...result.incorrect_answers, result.correct_answer].sort(() => Math.random() - 0.5).map(answer => {
                     return {
                         value: answer,
                         id: nanoid(),
@@ -34,16 +37,13 @@ function Quizpage() {
     }
 
     const holdAnswer = (id) => {
-
         setQuiz(prevState => prevState.map(quiz => {
             return {
                 question: quiz.question,
-                answers: quiz.answers.map(answer => answer.id === id ? { ...answer, isHeld: !answer.isHeld } : answer)
+                answers: quiz.answers.map(answer => answer.id === id ? { ...answer, isHeld: !answer.isHeld } : answer )
             }
         }))
-        
     }
-
 
     const checkAnswer = () => {
         setQuiz(prevState => prevState.map(quiz => {
@@ -81,6 +81,13 @@ function Quizpage() {
             <svg className="blob-1" width="126" height="131" viewBox="0 0 126 131" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M63.4095 71.3947C35.1213 40.8508 -2.68211 11.7816 1.17274 -29.6933C5.43941 -75.599 39.854 -115.359 82.4191 -133.133C122.797 -149.994 170.035 -140.256 205.822 -115.149C235.947 -94.0141 236.823 -53.8756 246.141 -18.271C256.17 20.0508 282.521 60.8106 260.501 93.7792C237.538 128.159 188.991 133.432 147.931 128.768C112.318 124.723 87.7505 97.6768 63.4095 71.3947Z" fill="#FFFAD1"/>
             </svg>
+
+            { numCorrectAns === quiz.length && (
+                <Confetti 
+                    recycle={false}
+                    numberOfPieces={1000}
+                />
+            )}
             
             <div className="quizzes_container">
                 {quizzes}
